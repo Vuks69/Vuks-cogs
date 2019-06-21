f94d890dc959da7cbc72847417091910131e8a31
Vuks <51289041+Vuks69@users.noreply.github.com>
2019-06-22 00:18:56 +0200

    done the phonetics dict

--- phonetics/phonetics.py
+++ phonetics/phonetics.py
@@ -11,7 +11,29 @@ class Phonetics(commands.Cog):
             'A': ('Alfa', '__AL__ FAH'),
             'B': ('Bravo', '__BRAH__ VOH'),
             'C': ('Charlie', '__CHAR__ LEE or __SHAR__ LEE'),
-            'D': ('Delta', '__DELL__ TAH')
+            'D': ('Delta', '__DELL__ TAH'),
+            'E': ('Echo', '__ECK__ OH'),
+            'F': ('Foxtrot', '__FOKS__ TROT'),
+            'G': ('Golf', 'GOLF'),
+            'H': ('Hotel', 'HO __TELL__'),
+            'I': ('India', '__IN__ DEE AH'),
+            'J': ('Juliett', '__JEW__ LEE ETT'),
+            'K': ('Kilo', '__KEY__ LOH'),
+            'L': ('Lima', '__LEE__ MAH'),
+            'M': ('Mike', 'MIKE'),
+            'N': ('November', 'NO __VEM__ BER'),
+            'O': ('Oscar', '__OSS__ CAH'),
+            'P': ('Papa', 'PAH __PAH__'),
+            'Q': ('Quebec', 'KEH __BECK__'),
+            'R': ('Romeo', '__ROW__ ME OH'),
+            'S': ('Sierra', 'SEE __AIR__ RAH'),
+            'T': ('Tango', '__TANG__ GO'),
+            'U': ('Uniform', '__YOU__ NEE FORM or __OO__ NEE FORM'),
+            'V': ('Victor', '__VIK__ TAH'),
+            'W': ('Whiskey', '__WISS__ KEY'),
+            'X': ('X-ray', '__ECKS__ RAY'),
+            'Y': ('Yankee', '__YANG__ KEY'),
+            'Z': ('Zulu', '__ZOO__ LOO')
         }
 
     @commands.command()
