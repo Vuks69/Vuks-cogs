17262cfaf792b1116356fb0236451f097093fa56
Vuks <51289041+Vuks69@users.noreply.github.com>
2020-09-27 21:46:04 +0200

    add config init

--- todo/commands.py
+++ todo/commands.py
@@ -1,9 +1,16 @@
-from redbot.core import commands
+import hashlib
+
+from redbot.core import commands, Config
 
 
 class Todo(commands.Cog):
+    __author__ = "Vuks#5767"
+
     def __init__(self, bot):
         self.bot = bot
+        unique_id = int(hashlib.sha512((self.__author__ + "@" + self.__class__.__name__).encode()).hexdigest(), 16)
+        self.config = Config.get_conf(self, identifier=unique_id, force_registration=True)
+        self.config.register_user(todos=[])
 
     @staticmethod
     async def red_delete_data_for_user(*, requester, user_id):
