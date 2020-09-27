881d713bc8a96a024d0bbf41d72823f98707c01c
Vuks <51289041+Vuks69@users.noreply.github.com>
2020-09-27 23:19:40 +0200

    implement todo add

--- todo/commands.py
+++ todo/commands.py
@@ -1,4 +1,7 @@
 import hashlib
+import discord
+
+from typing import Literal
 
 from redbot.core import commands, Config
 
@@ -13,17 +16,20 @@ class Todo(commands.Cog):
         self.config = Config.get_conf(self, identifier=unique_id, force_registration=True)
         self.config.register_user(todos=[])
 
-    @staticmethod
-    async def red_delete_data_for_user(*, requester, user_id):
-        NotImplementedError()
+    async def red_delete_data_for_user(
+            self, *, requester: Literal["discord", "owner", "user", "user_strict"], user_id: int,
+    ):
+        await self.config.user_from_id(user_id).clear()
 
     @commands.group()
     async def todo(self, ctx: commands.Context):
         NotImplementedError()
 
     @todo.command(aliases=["new", "create"])
-    async def add(self, ctx: commands.Context):
-        NotImplementedError()
+    async def add(self, ctx: commands.Context, new_todo: str):
+        async with self.config.user(ctx.author).todos() as user_todos:
+            user_todos.append(new_todo)
+        await ctx.tick()
 
     @todo.command(aliases=["delete", "del"])
     async def remove(self, ctx: commands.Context):
