5c9b1466c52d2cde8fa5a019b47bb8006ce3e4a9
Vuks <51289041+Vuks69@users.noreply.github.com>
2020-09-29 00:25:37 +0200

    add confirmation on todo clear

--- todo/commands.py
+++ todo/commands.py
@@ -1,9 +1,12 @@
+import asyncio
 import hashlib
 import discord
 
 from typing import Literal
 
 from redbot.core import commands, Config
+from redbot.core.utils.menus import start_adding_reactions
+from redbot.core.utils.predicates import ReactionPredicate
 
 
 class Todo(commands.Cog):
@@ -35,15 +38,27 @@ class Todo(commands.Cog):
     @todo.command(name="clear")
     async def remove_all(self, ctx: commands.Context):
         """Clear out your todos list"""
-        async with self.config.user(ctx.author).todos() as user_todos:
-            user_todos.clear()
-        await ctx.tick()
+        msg = await ctx.send("Are you sure you want to clear your todo list? This cannot be undone!")
+        # noinspection PyAsyncCall
+        start_adding_reactions(msg, ReactionPredicate.YES_OR_NO_EMOJIS)
+        pred = ReactionPredicate.yes_or_no(msg, ctx.author)
+        try:
+            await ctx.bot.wait_for("reaction_add", check=pred, timeout=60)
+        except asyncio.TimeoutError:
+            return
+
+        if not pred.result:
+            await ctx.send("Not clearing your todos.")
+        else:
+            async with self.config.user(ctx.author).todos() as user_todos:
+                user_todos.clear()
+            await ctx.send("Todos cleared.")
 
     @todo.command()
     async def list(self, ctx: commands.Context):
         """Show your personal todos."""
-        # https://github.com/aikaterna/gobcog/blob/7721dbfb96622d84ceb3e0dc8c7b60e13a592423/adventure/adventure.py#L3611
-        NotImplementedError()
+        # TODO
+        pass
 
     @todo.command(name="todo", hidden=True)
     async def _easter_egg(self, ctx: commands.Context):
